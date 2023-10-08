09da3032c064cb6ceecc6e9634df0d8177a1b184
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-08 22:46:58 +0500

    update

--- models_eval/run.sh
+++ models_eval/run.sh
@@ -10,20 +10,20 @@
 # python3 whisper_eval.py
 
 # 10 concurrent processes
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py &
-python3 whisper_eval.py
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py &
+# python3 whisper_eval.py
 
 
 
-# python3 bert_base_eval.py
+python3 bert_base_eval.py
 # python3 roberta_base_eval.py
 
 # python3 bert_base_eval.py & python3 roberta_base_eval.py
