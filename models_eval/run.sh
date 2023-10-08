ac37a52bd40a860e5ec5fc6f1277393026c76cce
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-08 20:52:33 +0500

    update

--- models_eval/run.sh
+++ models_eval/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # python3 bert_base_eval.py
-# python3 roberta_base_eval.py
+python3 roberta_base_eval.py
 
 # python3 bert_base_eval.py & python3 roberta_base_eval.py
 
-python3 roberta_base_eval.py & python3 roberta_base_eval.py
+# python3 roberta_base_eval.py & python3 roberta_base_eval.py
