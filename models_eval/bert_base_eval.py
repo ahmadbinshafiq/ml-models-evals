5ba4e6aea80ce23a9b4ad07affba32eb832f5262
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-08 20:07:20 +0500

    update

--- models_eval/bert_base_eval.py
+++ models_eval/bert_base_eval.py
@@ -1,3 +1,5 @@
+import time
+
 from transformers import BertTokenizer, BertModel
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
@@ -6,10 +8,18 @@ model = BertModel.from_pretrained("bert-base-uncased")
 X = 1000
 
 run_count: int = 0
+start_time = time.time()
 while run_count < X:
     text = "Replace me by any text you'd like."
     encoded_input = tokenizer(text, return_tensors="pt")
     output = model(**encoded_input)
-    print(f"Run {run_count} completed!", flush=True)
+    print(f"bert_base_uncased  --  Run {run_count} completed!", flush=True)
     # print(f"output: {output}", flush=True)
     run_count += 1
+
+end_time = time.time()
+print(
+    f"bert_base_uncased  --  Total time taken: {end_time - start_time} seconds",
+    flush=True,
+)
+
--- models_eval/run.sh
+++ models_eval/run.sh
@@ -3,4 +3,6 @@
 # python3 bert_base_eval.py
 # python3 roberta_base_eval.py
 
-python3 bert_base_eval.py & python3 roberta_base_eval.py
+# python3 bert_base_eval.py & python3 roberta_base_eval.py
+
+python3 roberta_base_eval.py & python3 roberta_base_eval.py
