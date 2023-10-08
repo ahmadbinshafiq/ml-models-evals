146836f599d4ab21b734aa724a6a9aafded992ce
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-08 22:03:31 +0500

    update

--- models_eval/run.sh
+++ models_eval/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# python3 whisper_eval.py
+
+# 5 concurrent processes
+python3 whisper_eval.py &
+python3 whisper_eval.py &
+python3 whisper_eval.py &
+python3 whisper_eval.py &
 python3 whisper_eval.py
+
 # python3 bert_base_eval.py
 # python3 roberta_base_eval.py
 
