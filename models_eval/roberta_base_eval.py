4a356e0820e53f21773438079d99944fdad681f5
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-08 19:58:01 +0500

    feat: add new roberts_base model for eval

---
+++ models_eval/roberta_base_eval.py
@@ -0,0 +1,15 @@
+from transformers import BertTokenizer, BertModel
+
+tokenizer = BertTokenizer.from_pretrained("roberta-base")
+model = BertModel.from_pretrained("roberta-base")
+
+X = 1000
+
+run_count: int = 0
+while run_count < X:
+    text = "Replace me by any text you'd like."
+    encoded_input = tokenizer(text, return_tensors="pt")
+    output = model(**encoded_input)
+    print(f"Run {run_count} completed!", flush=True)
+    # print(f"output: {output}", flush=True)
+    run_count += 1
--- models_eval/run.sh
+++ models_eval/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 bert_base_eval.py
+# python3 bert_base_eval.py
+python3 roberta_base_eval.py
