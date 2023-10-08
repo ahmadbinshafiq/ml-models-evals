35f1a1a3217643efdeff8d24b627a6e2bb042f39
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-08 21:59:58 +0500

    update

--- models_eval/whisper_eval.py
+++ models_eval/whisper_eval.py
@@ -1,21 +1,39 @@
+import time
+
 import whisper
 
 model = whisper.load_model("base")
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("./audios/ahmad.mp3")
-audio = whisper.pad_or_trim(audio)
+X = 10
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+run_count: int = 0
 
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+start_time = time.time()
+while run_count < X:
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio("./audios/ahmad.mp3")
+    audio = whisper.pad_or_trim(audio)
 
-# decode the audio
-options = whisper.DecodingOptions(fp16=False)
-result = whisper.decode(model, mel, options)
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-# print the recognized text
-print(result.text)
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+    # print the recognized text
+    # print(result.text)
+
+    print(f"whisper  --  Run {run_count} completed!", flush=True)
+    run_count += 1
+
+
+end_time = time.time()
+print(
+    f"whisper  --  Total time taken: {end_time - start_time} seconds for {X} runs",
+    flush=True,
+)
+
