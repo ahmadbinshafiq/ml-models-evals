bd2a46a5bec9a6a637d34db23cd955d58284df55
ahmadbinshafiq <ahmad.binshafiq529@gmail.com>
2023-10-09 01:17:38 +0500

    update

--- models_eval/run.sh
+++ models_eval/run.sh
@@ -23,7 +23,7 @@
 
 
 
-# python3 bert_base_eval.py
+python3 bert_base_eval.py
 
 # 5 concurrent processes
 # python3 bert_base_eval.py & 
@@ -33,16 +33,16 @@
 # python3 bert_base_eval.py
 
 # 10 concurrent processes
-python3 bert_base_eval.py & 
-python3 bert_base_eval.py &
-python3 bert_base_eval.py &
-python3 bert_base_eval.py &
-python3 bert_base_eval.py &
-python3 bert_base_eval.py & 
-python3 bert_base_eval.py &
-python3 bert_base_eval.py &
-python3 bert_base_eval.py &
-python3 bert_base_eval.py
+# python3 bert_base_eval.py & 
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py & 
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py &
+# python3 bert_base_eval.py
 
 
 
